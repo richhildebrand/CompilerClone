8c4e73b0b7ad8ff5d7bf2afb26c9dfeb1032a594
Rich <Rich@rich.com>
2012-08-30 17:18:49 -0400

    now displays on localhost

--- DataAccess/MongoDB/MongoConnectionFactory.cs
+++ DataAccess/MongoDB/MongoConnectionFactory.cs
@@ -8,7 +8,8 @@ namespace Compilify.DataAccess.MongoDB
 
         public MongoConnectionFactory(string connectionString)
         {
-            this.connectionString = connectionString;
+            //this.connectionString = connectionString;
+           this.connectionString = "mongodb://appharbor_46b59e98-aff4-463f-90ac-89aea8d0ac8f:mnu5pi8h4co8en6ftdjp478qsi@ds037407-a.mongolab.com:37407/appharbor_46b59e98-aff4-463f-90ac-89aea8d0ac8f";
         }
 
         public MongoDatabase Create()
